c44981b0973b67aafc500493dc97715a0e4f7089
blokkies48 <francisjlloyd15@gmail.com>
2022-09-08 19:47:29 +0200

    fixed Task_5

--- Level_1/Task_5/manipulation.py
+++ Level_1/Task_5/manipulation.py
@@ -13,3 +13,15 @@ print(str_manip[-1:-4:-1])
 
 #Getting the first 3 letters and last 2 and printing them
 print(str_manip[0:3] + str_manip[-2:])
+
+#Printing each letter to a new line
+print(str_manip.replace(" ","\n"))
+
+##Reviewer said letter and question said word not sure what. Question isn't detailed enough I though it ment print out all previous words created on a new line
+### Not sure if we are suposed to use for loops because it hasn't been covered yet
+
+#Printing out each letter
+for letter in str_manip:
+    if letter != " ":
+        print(letter)
+    
--- Level_1/Task_5/strings.py
+++ Level_1/Task_5/strings.py
@@ -2,6 +2,5 @@
 #Declaring string variable
 hero = "Super Man"
 
-#Printing hero string with ^ inbetween letters and making it all caps.
-print((hero[0] + "^" + hero[1] + "^" + hero[2] + "^"+ hero[3] + "^"+ hero[4] +  hero[5] +  hero[6] + "^"+ hero[7] + "^"+ hero[8]).upper())
-
+#Printing hero string with ^ inbetween letters using replace() and making it all caps.
+print((hero[0] + hero[1:4].replace("", "^") + hero[4:7] + hero[7:8].replace("", "^") + hero[-1]).upper())
